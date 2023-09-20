6db549efce43d41f07899594cff7756e3618f233
Sanjar Umarov <usanjar31@gmail.com>
2023-09-20 08:43:13 +0500

    created controller package

--- src/main/java/com/skillbox/users/controller/HelloController.java
+++ src/main/java/com/skillbox/users/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.skillbox.users;
+package com.skillbox.users.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
